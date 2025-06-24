5b6bc870d3a5c421c6457673cfe66053640646c3
SiddhiM4 <siddhimandekar47@gmail.com>
2025-06-24 12:58:20 +0530

    vowel or not prog added

---
+++ VowelOrNot.java
@@ -0,0 +1,10 @@
+class Solution {
+    String isVowel(char c) {
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+            c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+}
